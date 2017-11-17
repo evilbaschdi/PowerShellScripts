3e8fd9dc015f96d92360f83569450739e78dd49b
Sebastian Walter <sebwalter@gmx.net>
2017-11-17 10:32:14 +0100

    changed repo name for write-host output

--- PowerShellProject/clone.ps1
+++ PowerShellProject/clone.ps1
@@ -3,7 +3,7 @@ $Type = "users"
 $Repos = (Invoke-RestMethod -uri https://api.github.com/$Type/$Name/repos)
 
 foreach ($Repo in $Repos) {
-    Write-Host $Repo.clone_url
+    Write-Host $Repo.name 
     git clone $Repo.clone_url
     Write-Host ---
 }
