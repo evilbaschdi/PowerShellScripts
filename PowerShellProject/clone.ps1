eecb6da7718c62ef2df8058ec4cc7303d4145ded
Sebastian Walter <sebwalter@gmx.net>
2017-11-23 07:40:23 +0100

    clone

--- PowerShellProject/clone.ps1
+++ PowerShellProject/clone.ps1
@@ -1,9 +1,10 @@
 ﻿$Name = "evilbaschdi"
 $Type = "users"
+
 $Repos = (Invoke-RestMethod -uri https://api.github.com/$Type/$Name/repos)
 
 foreach ($Repo in $Repos) {
-    Write-Host $Repo.git_url
+    Write-Host $Repo.name
     git clone $Repo.git_url
     Write-Host ---
 }
---
+++ PowerShellProject/clone_vsts.ps1
@@ -0,0 +1,14 @@
+﻿$Name = Read-Host "Account"
+$Collection = Read-Host "Collection"
+if($Collection -eq $null){"DefaultCollection"}
+$username = Read-Host "Username"
+$password = Read-Host "Password"
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+
+$Repos = (Invoke-RestMethod -uri https://$Name.visualstudio.com/$Collection/_apis/git/repositories?api-version=1.0 -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)})
+
+foreach ($Repo in $Repos.value) {
+    Write-Host $Repo.name
+    git clone $Repo.remoteUrl
+    Write-Host ---
+}
