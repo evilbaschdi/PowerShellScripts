1f23a5a6dea4105f458c40cedb58e23269505ba6
Sebastian Walter <sebwalter@gmx.net>
2019-09-27 08:01:32 +0200

    corrected clone script

--- PowerShellProject/clone.ps1
+++ PowerShellProject/clone.ps1
@@ -5,7 +5,6 @@ $Repos = (Invoke-RestMethod -uri https://api.github.com/$Type/$Name/repos)
 
 foreach ($Repo in $Repos) {
     Write-Output $Repo.name
-    git clone $Repo.git_url
+    git clone $Repo.clone_url
     Write-Output ---
 }
-
