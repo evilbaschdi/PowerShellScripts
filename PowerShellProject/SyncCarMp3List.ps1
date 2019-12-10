8f9e8899e8b1693c813a75f17ac50a548e9f926b
Sebastian Walter <sebwalter@gmx.net>
2019-12-10 08:01:04 +0100

    Added SyncCarMp3List draft

---
+++ PowerShellProject/SyncCarMp3List.ps1
@@ -0,0 +1,15 @@
+﻿[xml]$XmlDocument = Get-Content "D:\Users\evilb\OneDrive\Dokumente\Audi.ffs_gui"
+
+$SyncPair = $XmlDocument.CreateElement("Pair")
+$Left = $XmlDocument.CreateElement("Left")
+$Right = $XmlDocument.CreateElement("Right")
+
+$Left.InnerText = "TestLinks"
+$Right.InnerText = "TestRechts"
+
+$SyncPair.AppendChild($Left)
+$SyncPair.AppendChild($Right)
+
+$XmlDocument.FreeFileSync.FolderPairs.AppendChild($SyncPair)
+
+$XmlDocument.InnerXml
