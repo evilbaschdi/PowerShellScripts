c1f668a5ef915ea1b265d07d6cbafb55bb6f9668
Sebastian Walter <sebwalter@gmx.net>
2023-01-01 10:51:31 +0100

    - removed older nuget based scripts and replaced content of BuildAndDeploy to call publish.ps1 - renamed git scripts to have "Git" as prefix - added backup command to iobroker scripts - added mp3 sync files

---
+++ PowerShellProject/GitLocalBranchesWithChanges.ps1
@@ -0,0 +1,23 @@
+Param([string]$ProjectsPath = 'C:\Git')
+$NothingToCommit = 'nothing to commit'
+$BranchAhead = 'Your branch is ahead'
+
+$ChangesToCommitArray = New-Object System.Collections.ArrayList
+$BranchAheadArray = New-Object System.Collections.ArrayList
+
+ForEach ($Directory in Get-ChildItem -Path $ProjectsPath) {
+    If ($Directory.PSIsContainer -eq $True) {
+        Set-Location $Directory.FullName
+        If (Test-Path .\.git) {
+            $GitDiff = git diff HEAD
+            If ($GitDiff -inotlike "") {
+                Write-Output $Directory.FullName
+            }
+        }
+    }
+}
+
+Set-Location $PSScriptRoot
+Write-Output
+Write-Output -NoNewLine "Press any key to continue..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
--- PowerShellProject/iobroker_UpdateHost.ps1
+++ PowerShellProject/iobroker_UpdateHost.ps1
@@ -1,5 +1,5 @@
 ﻿Find-Module PoSH-SSH | Install-Module -Scope CurrentUser
 New-SSHSession -ComputerName x-raspberrypi1 -Credential pi
 
-$stream = Invoke-SSHCommand -SessionId 0 -Command "iobroker stop && iobroker update && iobroker upgrade self && iobroker start"
-#Write-Information $sshCommand.Read()
+$stream = Invoke-SSHCommand -SessionId 0 -Command "cd /opt/iobroker && iob backup && iob stop && iob update && iob upgrade self && iob fix && iob start"
+Write-Information $stream
